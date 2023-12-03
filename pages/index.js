11b00be60217e4b3980e354fd0d351c8f0357dae
saulotj <135673362+saulotj@users.noreply.github.com>
2023-12-03 17:11:40 +0000

    Receita de bolo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,48 @@
 function Home() {
-    return <h1>Mo te ama muito!! ❤️</h1>
-}
+    return( 
+    <div>
+    <h1>Mo te ama muito!! ❤️</h1>
+    <h2>Receita de bolo</h2>
+    <h3>Para a massa</h3>
+    <li>
+        <ul>3 ovos</ul>
+        <ul>1 xícara e meia de chá de açúcar</ul>
+        <ul>Meia xícara de chá de óleo</ul>
+        <ul>1 xícara de chá de chocolate em pó</ul>
+        <ul>2 xícaras de chá de farinha de trigo</ul>
+        <ul>1 xícara de chá de água quente</ul>
+        <ul>1 colher de sopa de fermento em pó</ul>
+    </li>
+    <h2>Modo de Preparo</h2>
+    <h3>Massa</h3>
+    <li>
+        <ol>
+Em uma tigela, coloque 3 ovos, 1 e meia xícara de chá de açúcar, meia xícara de chá de óleo, 1 xícara de chá de chocolate em pó e 2 xícaras de chá de farinha de trigo. 
+Misture delicadamente os ingredientes.</ol>
+        <ol>
+Em seguida, adicione 1 xícara de chá de água quente, 
+1 colher de sopa de fermento em pó e bata até ficar homogêneo.</ol>
+        <ol>Transfira a massa para uma forma untada e enfarinhada com uma mistura de farinha de trigo e chocolate em pó. Leve para assar 
+            em forno preaquecido a 180 graus Celsius por 40 minutos.</ol>
+    </li>
 
+    <h2>ingredientes</h2>
+    <h3>Cobertura</h3>
+    <li>
+        <ul>1 xícara e meia de chá de leite</ul>
+        <ul>Meia xícara de chá de chocolate em pó</ul>
+        <ul>1 colher de sopa de manteiga</ul>
+        <ul>1 xícara de chá de açúcar</ul>
+    </li>
+
+    <h2>Modo de Preparo</h2>
+    <h3>Cobertura</h3>
+    <li>
+        <ol>Em uma panela, coloque 1 e meia xícara de chá de leite, meia xícara de chá de chocolate em pó, 1 colher de sopa de manteiga e 1 xícara de chá de açúcar.
+             Misture, ligue o fogo e deixe ferver.</ol>
+        <ol>Despeje a calda no bolo ainda quente e sirva em seguida.</ol>
+    </li>
+    </div>   
+    );
+}
 export default Home;
