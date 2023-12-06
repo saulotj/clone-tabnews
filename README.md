e5a0909e23121e2ae8732b9f63334d321c1073fc
saulotj <135673362+saulotj@users.noreply.github.com>
2023-12-06 21:12:28 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Clone da implementação do tabnews  do curso.dev
+
+Clone da implementação do tabnews do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return( 
+  return (
     <div>
-    <h1>Te amo muito! ❤️</h1>
-    <h2>Receita de bolo</h2>
-    <h3>Para a massa</h3>
-    <ul>
+      <h1>Te amo muito! ❤️</h1>
+      <h2>Receita de bolo</h2>
+      <h3>Para a massa</h3>
+      <ul>
         <li>3 ovos</li>
         <li>1 xícara e meia de chá de açúcar</li>
         <li>Meia xícara de chá de óleo</li>
@@ -12,39 +12,54 @@ function Home() {
         <li>2 xícaras de chá de farinha de trigo</li>
         <li>1 xícara de chá de água quente</li>
         <li>1 colher de sopa de fermento em pó</li>
-    </ul>
-    <h2>Modo de Preparo</h2>
-    <h3>Massa</h3>
-    <ol>
+      </ul>
+      <h2>Modo de Preparo</h2>
+      <h3>Massa</h3>
+      <ol>
         <li>
-Em uma tigela, clioque 3 ovos, 1 e meia xícara de chá de açúcar, meia xícara de chá de óleo, 1 xícara de chá de chocliate em pó e 2 xícaras de chá de farinha de trigo. 
-Misture delicadamente os ingredientes.</li>
+          Em uma tigela, clioque 3 ovos, 1 e meia xícara de chá de açúcar, meia
+          xícara de chá de óleo, 1 xícara de chá de chocliate em pó e 2 xícaras
+          de chá de farinha de trigo. Misture delicadamente os ingredientes.
+        </li>
         <li>
-Em seguida, adicione 1 xícara de chá de água quente, 
-1 clolher de sopa de fermento em pó e bata até ficar homogêneo.</li>
-        <li>Transfira a massa para uma forma untada e enfarinhada com uma mistura de farinha de trigo e chocolate em pó. Leve para assar 
-            em forno preaquecido a 180 graus Celsius por 40 minutos.</li>
-    </ol>
+          Em seguida, adicione 1 xícara de chá de água quente, 1 clolher de sopa
+          de fermento em pó e bata até ficar homogêneo.
+        </li>
+        <li>
+          Transfira a massa para uma forma untada e enfarinhada com uma mistura
+          de farinha de trigo e chocolate em pó. Leve para assar em forno
+          preaquecido a 180 graus Celsius por 40 minutos.
+        </li>
+      </ol>
 
-    <h2>ingredientes</h2>
-    <h3>Cobertura</h3>
-    <ul>
+      <h2>ingredientes</h2>
+      <h3>Cobertura</h3>
+      <ul>
         <li>1 xícara e meia de chá de leite</li>
         <li>Meia xícara de chá de chocolate em pó</li>
         <li>1 colher de sopa de manteiga</li>
         <li>1 xícara de chá de açúcar</li>
-    </ul>
+      </ul>
 
-    <h2>Modo de Preparo</h2>
-    <h3>Cobertura</h3>
-    <ol>
-        <li>Em uma panela, coloque 1 e meia xícara de chá de leite, meia xícara de chá de chocolate em pó, 1 colher de sopa de manteiga e 1 xícara de chá de açúcar.
-             Misture, ligue o fogo e deixe ferver.</li>
+      <h2>Modo de Preparo</h2>
+      <h3>Cobertura</h3>
+      <ol>
+        <li>
+          Em uma panela, coloque 1 e meia xícara de chá de leite, meia xícara de
+          chá de chocolate em pó, 1 colher de sopa de manteiga e 1 xícara de chá
+          de açúcar. Misture, ligue o fogo e deixe ferver.
+        </li>
         <li>Despeje a calda no bolo ainda quente e sirva em seguida.</li>
-    </ol>
+      </ol>
 
-    <h3>Espero que seja boa, nunca testei peguei em algum link aleatorio da net</h3>
-    </div>   
-    );
+      <h3>
+        Espero que seja boa, nunca testei peguei em algum link aleatorio da net
+      </h3>
+    </div>
+  );
+}
+
+function teste() {
+  console.log("a");
 }
 export default Home;
